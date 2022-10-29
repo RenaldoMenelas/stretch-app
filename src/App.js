b6252a7f55b14ee5d903cef2b39df38464f7b800
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2022-10-29 12:09:15 -0400

    Homepage edits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './Components/Header'
 import Content from './Components/Content'
 import StretchContent from './StretchContent'
-
+import
 function App() {
   return (
     <div className="App">
